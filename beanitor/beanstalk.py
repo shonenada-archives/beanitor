e11102d7738583fde7101ae902874d35cc0d12f2
shonenada <shonenada@gmail.com>
2016-01-19 15:46:34 +0800

    catch SocketError

--- beanitor/beanstalk.py
+++ beanitor/beanstalk.py
@@ -1,4 +1,4 @@
-from beanstalkc import Connection
+from beanstalkc import Connection, SocketError
 
 
 class _BeanstalkState(object):
@@ -12,10 +12,13 @@ class Beanstalkc(object):
     '''A simple proxy object for beanstalk.'''
 
     def __init__(self, app=None):
+        self._client = None
+        self._connected = False
+
         if app is not None:
             self.init_app(app)
 
-    def init_app(self, app):
+    def init_app(self, app, silent=True):
         app.config.setdefault('BEANSTALK_HOST', '127.0.0.1')
         app.config.setdefault('BEANSTALK_PORT', 11300)
 
@@ -23,9 +26,20 @@ class Beanstalkc(object):
             app.extensions = {}
         app.extensions['beanstalk'] = _BeanstalkState(self, app)
 
-        self._client = Connection(
-            host=app.config.get('BEANSTALK_HOST'),
-            port=app.config.get('BEANSTALK_PORT'))
+        try:
+            self._client = Connection(
+                host=app.config.get('BEANSTALK_HOST'),
+                port=app.config.get('BEANSTALK_PORT'))
+            self._connected = True
+        except SocketError:
+            if silent:
+                self._client = None
+                self._connected = False
+            else:
+                raise
 
     def __getattr__(self, name):
         return getattr(self._client, name)
+
+    def is_connected(self):
+        return self._connected is True
